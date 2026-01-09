252d0474d9bd4864a843b5a9ad0edb0d95a1d4c4
Andre de Paula <andrepps22@gmail.com>
2026-01-09 14:18:16 -0300

    feat: Implementado o metodo de login  usando Command AuthViewModel

---
+++ lib/view_models/auth_view_model.dart
@@ -0,0 +1,41 @@
+import 'package:chat/core/utils/result.dart';
+import 'package:chat/data/domain/DTOs/user_dto.dart';
+import 'package:chat/data/domain/interfaces/i_auth_repository.dart';
+import 'package:chat/view_models/command.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthViewModel with ChangeNotifier {
+  late final Command authLoginCommand;
+  final IAuthRepository _authRepository;
+  UserDto? userDto;
+  bool islogged = false;
+  String? errorMessage;
+
+  AuthViewModel({required IAuthRepository authRepository}):_authRepository = authRepository {
+    authLoginCommand = Command(() => _authRepository.login(userDto!),);
+
+    authLoginCommand.addListener(notifyListeners);
+  }
+
+  @override
+  void dispose() {
+    authLoginCommand.removeListener(notifyListeners);
+    super.dispose();
+  }
+
+
+  Future<void> login() async {
+    await authLoginCommand.execute();
+
+    final Result<User?, Exception> result = authLoginCommand.data;
+
+    result.when(
+      success: (value) {
+        islogged = true;
+      }, 
+      failure: (exception) => errorMessage = exception.toString(),);
+
+  }
+
+}
