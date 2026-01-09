c14834d79199184d27fa967b218944229eb621c3
Andre de Paula <andrepps22@gmail.com>
2026-01-09 15:08:12 -0300

    refactor: Alterado a rota para para apontar para o provider local da pagina

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,11 +1,11 @@
 import 'package:chat/core/router/app_router_static.dart';
-import 'package:chat/view/auth_view/auth_view.dart';
+import 'package:chat/view/auth_view/auth_provider.dart';
 import 'package:chat/view/home/home_view.dart';
 import 'package:flutter/cupertino.dart';
 
 class AppRouter {
   Map<String, WidgetBuilder> router = {
-    AppRouterStatic.login: (ctx) => AuthView(),
+    AppRouterStatic.login: (ctx) => AuthProvider(),
     AppRouterStatic.home: (ctx) => HomeView()
     
   };
