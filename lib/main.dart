aaeaea4d4b4bff41ca9e8a7443481b2d73e1f965
Andre de Paula <andrepps22@gmail.com>
2026-01-09 15:10:42 -0300

    Refactor: Adicionando o multiprovider no main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:chat/core/provider/provider.dart';
+import 'package:chat/core/router/app_router.dart';
 import 'package:chat/core/theme/app_theme.dart';
 import 'package:chat/firebase_options.dart';
-import 'package:chat/view/auth_view/auth_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform
   );
-  runApp(const MainApp());
+  
+  runApp(MultiProvider(providers: Providers().provider, child: MainApp(),));
 }
 
 class MainApp extends StatefulWidget {
@@ -30,8 +33,9 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: AppRouter().router,
       theme: isDark? AppTheme.dark: AppTheme.light,
       debugShowCheckedModeBanner: false,
-      home: AuthView(funcao: alterarTema,));
+      );
   }
 }
--- lib/view/auth_view/auth_view.dart
+++ lib/view/auth_view/auth_view.dart
@@ -1,17 +1,20 @@
+import 'package:chat/core/router/app_router_static.dart';
+import 'package:chat/data/domain/DTOs/user_dto.dart';
 import 'package:chat/view/components/custom_buttom.dart';
 import 'package:chat/view/components/custom_textfield.dart';
+import 'package:chat/view_models/auth_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AuthView extends StatefulWidget {
-  final VoidCallback funcao;
-  const AuthView({super.key, required this.funcao});
+  const AuthView({super.key, });
 
   @override
   State<AuthView> createState() => _AuthViewState();
 }
 
 class _AuthViewState extends State<AuthView> {
-  final TextEditingController nome = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
   final TextEditingController email = TextEditingController();
   final TextEditingController senha = TextEditingController();
   
@@ -20,6 +23,7 @@ class _AuthViewState extends State<AuthView> {
 
   @override
   Widget build(BuildContext context) {
+    final vm = context.watch<AuthViewModel>();
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
@@ -40,7 +44,16 @@ class _AuthViewState extends State<AuthView> {
                     CustomTextfield(label: 'Senha',controller: senha, onSaved: onSaved , obscure: true,),
                     
                     SizedBox(height: 15,),
-                    CustomButtom(label: 'Entrar', onTap: widget.funcao,),
+                    CustomButtom(label: 'Entrar', onTap: (){
+                      
+                        vm.userDto = UserDto(email: email.text, password: senha.text);
+                        vm.login();
+
+                        if(vm.islogged){
+                          Navigator.of(context).pushNamed(AppRouterStatic.home);
+                        }
+                      
+                    },),
                     SizedBox(height: 10,),
                     TextButton(onPressed: (){}, child: Text('Criar uma nova conta', style: TextStyle(color: Theme.of(context).colorScheme.primary),),)
                   ],
