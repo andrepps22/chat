aff494c090b2e55e377861f9e12d73d88c54b8b2
Andre de Paula <andrepps22@gmail.com>
2026-01-11 17:47:59 -0300

    refact: adicionado o usercase e fireStone

--- lib/core/provider/provider.dart
+++ lib/core/provider/provider.dart
@@ -1,6 +1,10 @@
 import 'package:chat/data/domain/interfaces/i_auth_repository.dart';
+import 'package:chat/data/domain/interfaces/i_user_repository.dart';
+import 'package:chat/data/domain/use_cases/register_user_use_case.dart';
 import 'package:chat/data/repository/auth_repository_impl.dart';
+import 'package:chat/data/repository/user_repository_impl.dart';
 import 'package:chat/data/services/auth_firebase_services.dart';
+import 'package:chat/data/services/firestone_firebase_service.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
@@ -9,10 +13,20 @@ class Providers {
     return [
       //=============Services======================
       Provider<AuthServices>(create: (context) => AuthServices(),),
+      Provider<FirestoneFirebaseService>(create: (context) => FirestoneFirebaseService(),),
 
       //=============Repository====================
       ProxyProvider<AuthServices, IAuthRepository>(
-        update: (_, authServices, _) => AuthRepositoryImpl(authServices: authServices),)
+        update: (_, authServices, _) => AuthRepositoryImpl(authServices: authServices),),
+      ProxyProvider<FirestoneFirebaseService, IUserRepository>(
+        update: (_, firestoneService, _) => UserRepositoryImpl(firestoneService: firestoneService),),
+
+      //=============Use Cases====================
+      ProxyProvider2<IAuthRepository, IUserRepository, RegisterUserUseCase>(
+        update: (_, authRepo, userRepo, _) => RegisterUserUseCase(
+          authRepository: authRepo,
+          userRepository: userRepo,
+        ),),
       ];
   }
 }
